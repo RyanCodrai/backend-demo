b8db22f6d59520fef866aa95b2fd8de15f32e000
ryancodrai <ryan.codrai@gmail.com>
2023-08-14 15:45:01 +0100

    Update iso to unix time conversion to accept timezone offset data

--- src/utils.py
+++ src/utils.py
@@ -8,8 +8,9 @@ def read_api_version():
 
 def iso_to_unix(iso_time):
     try:
-        unix_timestamp = time.mktime(time.strptime(iso_time, "%Y-%m-%dT%H:%M:%S"))
+        iso_format = "%Y-%m-%dT%H:%M:%S%z"
+        unix_timestamp = time.mktime(time.strptime(iso_time, iso_format))
         return int(unix_timestamp)
     except ValueError:
-        raise ValueError("Invalid ISO time format")
+        raise ValueError("Invalid ISO time format with offset")
 
